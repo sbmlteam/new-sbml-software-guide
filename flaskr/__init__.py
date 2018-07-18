e400d21e9d49eaf398a3f89d64fa80206902c50e
winterbloom <hazel.r.pearson@gmail.com>
2018-07-17 17:29:34 -0700

    Added threading for concurrent access to Flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,6 @@ import os
 
 from flask import Flask
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -31,14 +30,20 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
         
-    from . import db
+    import db
     db.init_app(app)
     
-    from . import auth
+    import auth
     app.register_blueprint(auth.bp)
     
-    from . import entry
+    import entry
     app.register_blueprint(entry.bp)
     app.add_url_rule('/', endpoint='index')
         
     return app
+
+if __name__ == "__main__":
+    # print "(a)"
+    app = create_app()
+    print ("starting")
+    app.run(threaded = True)
