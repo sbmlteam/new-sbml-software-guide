5ff064f824dffe1b1da0db50a7c891dbf5bbee67
Michael Hucka <mhucka@caltech.edu>
2018-08-10 14:18:55 -0700

    Add a simple run.py file to start the system
    The idea for having a run.py file at all is based on some Flask guide
    such as https://damyanon.net/post/flask-series-structure/.  This is
    not required but it seems reasonable to add, as alternative.

---
+++ run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    run.py
+# @brief   Start file for SBML Software Guide server
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/sbmlteam/new-sbml-software-guide
+# =============================================================================
+
+import guide
+from guide.__main__ import main as main
+
+# Hand over to the command line interface.
+if __name__ == '__main__':
+	main()
