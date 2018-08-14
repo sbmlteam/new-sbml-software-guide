1c44dc83a9d7434645c5cb9b9ea551e249e41c4a
winterbloom <hazel.r.pearson@gmail.com>
2018-08-13 21:15:41 -0700

    Added the rest of the required form sections (ie: they're now writable and editable) but it doesn't yet display in a pretty format

--- flaskr/entrydata.py
+++ flaskr/entrydata.py
@@ -54,6 +54,93 @@ class Entry:
 		self.fee_govt = self.get_fee('fee_govt', form)
 		self.fee_commercial = self.get_fee('fee_commercial', form)
 
+		# open source checks
+		self.src = form['src']
+		self.src_other_txt = form['src_other_txt'] if self.src == "3" else ""
+
+		# sbml level
+		sbml_lvl = form.getlist('sbml_lvl')
+
+		self.lvl_1_1_i = "lvl_1_1_i" in sbml_lvl
+		self.lvl_1_2_i = "lvl_1_2_i" in sbml_lvl
+		self.lvl_2_1_i = "lvl_2_1_i" in sbml_lvl
+		self.lvl_2_2_i = "lvl_2_2_i" in sbml_lvl
+		self.lvl_2_3_i = "lvl_2_3_i" in sbml_lvl
+		self.lvl_2_4_i = "lvl_2_4_i" in sbml_lvl
+		self.lvl_2_5_i = "lvl_2_5_i" in sbml_lvl
+		self.lvl_3_1_i = "lvl_3_1_i" in sbml_lvl
+		self.lvl_3_2_i = "lvl_3_2_i" in sbml_lvl
+
+		self.lvl_1_1_e = "lvl_1_1_e" in sbml_lvl
+		self.lvl_1_2_e = "lvl_1_2_e" in sbml_lvl
+		self.lvl_2_1_e = "lvl_2_1_e" in sbml_lvl
+		self.lvl_2_2_e = "lvl_2_2_e" in sbml_lvl
+		self.lvl_2_3_e = "lvl_2_3_e" in sbml_lvl
+		self.lvl_2_4_e = "lvl_2_4_e" in sbml_lvl
+		self.lvl_2_5_e = "lvl_2_5_e" in sbml_lvl
+		self.lvl_3_1_e = "lvl_3_1_e" in sbml_lvl
+		self.lvl_3_2_e = "lvl_3_2_e" in sbml_lvl
+
+		# level 3 package checks
+		sbml_pkg = form.getlist("sbml_pkg")
+
+		self.pkg_array_r =		"pkg_array_r"	in sbml_pkg
+		self.pkg_distr_r =		"pkg_distr_r"	in sbml_pkg
+		self.pkg_flux_r =		"pkg_flux_r"	in sbml_pkg
+		self.pkg_groups_r =		"pkg_groups_r"	in sbml_pkg
+		self.pkg_heir_r =		"pkg_heir_r"	in sbml_pkg
+		self.pkg_layout_r =		"pkg_layout_r"	in sbml_pkg
+		self.pkg_multi_r =		"pkg_multi_r"	in sbml_pkg
+		self.pkg_quali_r =		"pkg_quali_r"	in sbml_pkg
+		self.pkg_render_r =		"pkg_render_r"	in sbml_pkg
+		self.pkg_spacial_r =	"pkg_spacial_r"	in sbml_pkg
+
+		self.pkg_array_w =		"pkg_array_w"	in sbml_pkg
+		self.pkg_distr_w =		"pkg_distr_w"	in sbml_pkg
+		self.pkg_flux_w =		"pkg_flux_w"	in sbml_pkg
+		self.pkg_groups_w =		"pkg_groups_w"	in sbml_pkg
+		self.pkg_heir_w =		"pkg_heir_w"	in sbml_pkg
+		self.pkg_layout_w =		"pkg_layout_w"	in sbml_pkg
+		self.pkg_multi_w =		"pkg_multi_w"	in sbml_pkg
+		self.pkg_quali_w =		"pkg_quali_w"	in sbml_pkg
+		self.pkg_render_w =		"pkg_render_w"	in sbml_pkg
+		self.pkg_spacial_w =	"pkg_spacial_w"	in sbml_pkg
+
+		self.pkg_array_f =		"pkg_array_f"	in sbml_pkg
+		self.pkg_distr_f =		"pkg_distr_f"	in sbml_pkg
+		self.pkg_flux_f =		"pkg_flux_f"	in sbml_pkg
+		self.pkg_groups_f =		"pkg_groups_f"	in sbml_pkg
+		self.pkg_heir_f =		"pkg_heir_f"	in sbml_pkg
+		self.pkg_layout_f =		"pkg_layout_f"	in sbml_pkg
+		self.pkg_multi_f =		"pkg_multi_f"	in sbml_pkg
+		self.pkg_quali_f =		"pkg_quali_f"	in sbml_pkg
+		self.pkg_render_f =		"pkg_render_f"	in sbml_pkg
+		self.pkg_spacial_f =	"pkg_spacial_f"	in sbml_pkg
+
+		uses = form.getlist("uses")
+
+		self.uses_model		= "uses_model"		in uses
+		self.uses_numsim	= "uses_numsim"		in uses
+		self.uses_analysis	= "uses_analysis"	in uses
+		self.uses_visual	= "uses_visual"		in uses
+		self.uses_integrate	= "uses_integrate"	in uses
+		self.uses_manage	= "uses_manage"		in uses
+		self.uses_convert	= "uses_convert"	in uses
+		self.uses_prog		= "uses_prog"		in uses
+		self.uses_net		= "uses_net"		in uses
+		self.uses_other		= "uses_other"		in uses
+
+		self.uses_other_txt = form["uses_other_txt"] if self.uses_other else ""
+
+		lib = form.getlist("lib")
+		self.lib_sbml	= "lib_sbml"	in lib
+		self.lib_jsbml	= "lib_jsbml"	in lib
+		self.lib_custom	= "lib_custom"	in lib
+		self.lib_idk	= "lib_idk"		in lib
+		self.lib_other	= "lib_other"	in lib
+
+		self.lib_other_txt = form["lib_other_txt"] if self.lib_other else ""
+
 	def __getitem__(self, arg):
 		return getattr(self, arg)
 
@@ -91,17 +178,17 @@ class Entry:
 
 	# inserts this entry into the database
 	def insert(self, db):
-		db.execute(
-			'INSERT INTO post'
-			' (author_id, name, contact, contact_me, version, site, descr, tags,'
-			' dependency, dependency_other, os, os_other, fee_academic, fee_nonprofit,'
-			' fee_govt, fee_commercial, created)'
-			' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-			(self.author_id, self.name, self.contact, self.contact_me, self.version, self.site, 
-			self.descr, self.tags, self.dependency, self.dependency_other, self.os, self.os_other,
-			self.fee_academic, self.fee_nonprofit, self.fee_govt, self.fee_commercial,
-			self.created)
-		)
+		# gets a list of every property this entry has
+		prop_name = [str(attr) for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__") and not attr == "error"]
+		prop_val = [self[attr] for attr in prop_name]
+
+		# inserts those properties and their respective values into the database
+		cmd = ("INSERT INTO post (" + ", ".join(prop_name) + ")" + 
+				" VALUES (" + ", ".join(len(prop_val)*('?',)) + ")")
+
+		print(cmd)
+
+		db.execute(cmd, tuple(prop_val))
 		db.commit()
 
 	# updates this entry in the database
@@ -116,14 +203,12 @@ class Entry:
 		else:
 			contact = self.contact
 
-		db.execute(
-			'UPDATE post SET'
-			' name = ?, contact = ?, contact_me = ?, version = ?, site = ?, descr = ?, tags = ?,'
-			' dependency = ?, dependency_other = ?, os = ?, os_other = ?, fee_academic = ?,'
-			' fee_nonprofit = ?, fee_govt = ?, fee_commercial = ?'
-			' WHERE rowid = ?',
-			(self.name, contact, self.contact_me, self.version, self.site, self.descr, self.tags,
-			self.dependency, self.dependency_other, self.os, self.os_other, self.fee_academic, 
-			self.fee_nonprofit, self.fee_govt, self.fee_commercial, id)
-		)
+		prop_name = [str(attr) for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__") and not attr == "error"]
+		prop_val = [self[attr] for attr in prop_name]
+		prop_val.append(id)
+
+		cmd = ("UPDATE post SET " + ", ".join([name + " = ?" for name in prop_name]) +
+				"WHERE rowid = ?")
+
+		db.execute(cmd, tuple(prop_val))
 		db.commit()
